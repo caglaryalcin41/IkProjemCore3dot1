464554f7a58399e17b36345403b06a9d853a2475
çağlar çağlar <caglaryalcin41@gmail.com>
2023-05-15 21:37:30 +0300

    41- bazı değişiklikler

--- IkProjemCore3dot1/Models/PersonelContext.cs
+++ IkProjemCore3dot1/Models/PersonelContext.cs
@@ -13,9 +13,10 @@ namespace IkProjemCore3dot1.Models
         public virtual DbSet<Unvan> Unvan { get; set; }
         public virtual DbSet<Roller> Roller { get; set; }
         public virtual DbSet<User> User { get; set; }
-        public virtual DbSet<WebItem> WebItem { get; set; }
+        public virtual DbSet<WebItem> WebItem { get; set; } 
         public virtual DbSet<ct_RolePrivilege> ct_RolePrivilege { get; set; }
         public virtual DbSet<ct_UserRole> ct_UserRole { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         { 
