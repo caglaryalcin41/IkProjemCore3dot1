c3bad63e694981beca858261c58046aa23af7387
çağlar çağlar <caglaryalcin41@gmail.com>
2023-05-16 23:05:01 +0300

    41-.

--- IkProjemCore3dot1/Areas/Admin/Data/LoginUser.cs
+++ IkProjemCore3dot1/Areas/Admin/Data/LoginUser.cs
@@ -1,7 +1,7 @@
 ﻿namespace IkProjemCore3dot1.Areas.Admin.Data
 {
     public class LoginUser
-    {
+    { 
         public static int userRole = 0;
     }
 }
---
+++ IkProjemCore3dot1/Areas/Admin/Data/SingletonTest.cs
@@ -0,0 +1,37 @@
+﻿namespace IkProjemCore3dot1.Areas.Admin.Data
+{
+    public class SingletonTest
+    {
+
+        private SingletonTest()
+        {
+            
+        }
+
+        static SingletonTest()
+        {
+            _nesne = new SingletonTest();
+        }
+
+        private static SingletonTest _nesne;
+
+        public static SingletonTest singletonTest { get { return _nesne; }   }
+
+    }
+}
+/*      
+ *      private SingletonTest()
+        {
+            
+        }
+
+        private static SingletonTest _singletonTest;
+
+        public static SingletonTest singletonTestNesnesiVer()
+        {
+            if (_singletonTest == null)
+            {
+                _singletonTest = new SingletonTest();
+            }
+            return _singletonTest;
+        }*/
